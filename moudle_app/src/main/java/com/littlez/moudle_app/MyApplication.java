675d95b4fc9aa9ebbaef9fef48ec10882ade9b1c
wangzhi <wangzhi_9211@163.com>
2019-09-17 17:08:42 +0800

    基础组件化方案搭建成功

---
+++ moudle_app/src/main/java/com/littlez/moudle_app/MyApplication.java
@@ -0,0 +1,11 @@
+package com.littlez.moudle_app;
+
+import android.app.Application;
+
+/**
+ * created by xiaozhi
+ * <p>
+ * Date 2019/9/17
+ */
+public class MyApplication extends Application {
+}
---
+++ moudle_map/src/main/java/debug/MapApplication.java
@@ -0,0 +1,11 @@
+package debug;
+
+import android.app.Application;
+
+/**
+ * created by xiaozhi
+ * <p>
+ * Date 2019/9/17
+ */
+public class MapApplication extends Application {
+}
